a39ac17ecda39661bb23841cf8fb5cdb4bfbf3d3
Jeff Daze <jdaze@hostopia.com>
2021-12-03 13:05:25 -0500

    Add small optimization

--- day1.js
+++ day1.js
@@ -1,21 +1,16 @@
 const data = require('./day1_data.js');
 
 let count = 0;
-
-for(let i = 0; i < data.length; i++)
-{
-  if (data[i] < data[i + 1])
-  {
-    count++;
-  }
-}
-
-console.log("Day 1 a Value:", count);
-
 let count2 = 0;
 
 for(let i = 0; i < data.length; i++)
 {
+  // first problem...
+  if (data[i] < data[i + 1])
+  {
+    count++;
+  }
+
   // calculate our window values...
   let window1 = data[i] + data[i + 1] + data[i + 2];
   let window2 = data[i + 1] + data[i + 2] + data[i + 3];
@@ -25,4 +20,5 @@ for(let i = 0; i < data.length; i++)
   }
 }
 
+console.log("Day 1 a Value:", count);
 console.log("Day 1 b Value:", count2);
