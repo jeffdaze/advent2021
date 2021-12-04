d661ff45e5e4ce0321514347c6c95cef9acb4020
Jeff Daze <jeffdaze@hotmail.com>
2021-12-03 22:41:37 -0500

    Add scaffold for day 4; still refactoring day 3 for upload (current is not fit for public consumption)

---
+++ day4.js
@@ -0,0 +1,19 @@
+const data = require('./day1_data.js');
+
+let count = 0;
+
+for(let i = 0; i < data.length; i++)
+{
+  console.log(data[i]);
+}
+
+console.log("Day 4 a Value:", count);
+
+let count2 = 0;
+
+for(let i = 0; i < data.length; i++)
+{
+  console.log(data[i]);
+}
+
+console.log("Day 4 b Value:", count2);
