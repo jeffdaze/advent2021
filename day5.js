da76c2944557da1ef57aef0c11aae0260d28bb4d
Jeff Daze <jeffdaze@hotmail.com>
2021-12-04 23:35:55 -0500

    Add day 5 scaffold... will eventually add actual code -- maybe?

---
+++ day5.js
@@ -0,0 +1,12 @@
+const data = require('./day1_data.js');
+
+let count = 0;
+let count2 = 0;
+
+for(let i = 0; i < data.length; i++)
+{
+  console.log(data[i])
+}
+
+console.log("Day 1 a Value:", count);
+console.log("Day 1 b Value:", count2);
